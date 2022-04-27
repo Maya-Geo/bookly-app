92f6500bee30ccfe8639ae5cf91a14d75a249af3
Maya Hagan <miyaclessa@gmail.com>
2022-04-27 01:59:56 +0000

    add home

---
+++ Components/BottomScreens/Home.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import Bottomnavigator from "../../Navigator/BottomNavigator";
+import React from "react";
+
+const Home = () => {
+  return <Bottomnavigator />;
+};
+export default Home;
+
+const styles = StyleSheet.create({});
